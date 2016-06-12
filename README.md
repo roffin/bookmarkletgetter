2e6a675cd6749283fc9cd960d12656a04b32ef89
Robin Törnquist <robin@Robins-MacBook-Pro.local>
2016-06-12 13:23:16 +0200

    delete button added next to config rows on options page

--- README.md
+++ README.md
@@ -36,11 +36,14 @@ This structure will then be downloaded to your browser when the extension perfor
 The folder will be added to the Bookmark Bar, but can be moved.
 
 ## Versions
+v1.3.0
+- Added "Delete" button next to config row(s) on options page
+
 v1.2.0
-- Added possibility to disable JavaScript injection.
+- Added possibility to disable JavaScript injection
 
 v1.1.1
-- Added cooldown time for synchronisation in order to fix a bug where bookmark(lets) were added multiple times.
+- Added cooldown time for synchronisation in order to fix a bug where bookmark(lets) were added multiple times
 
 v1.1.0:
 - New icon
--- src/options_custom/options.js
+++ src/options_custom/options.js
@@ -4,8 +4,45 @@ function getInjectScriptForSyncUri(uri) {
         if (injectScripts[i].syncUri == uri && injectScripts[i].injectScript)
             return injectScripts[i].injectScript;
     }
-
     return '';
+}
+
+var currentRowId = 0;
+function createConfigRow(folderName, syncServer, allowJavaScriptInjection) {
+    var configRowDiv = document.createElement('div');
+    configRowDiv.setAttribute('id', 'config-row-' + currentRowId);
+    configRowDiv.innerHTML = `<input type="text"
+                    placeholder="Folder name (optional)"
+                    class="sync-server-folder-name"
+                    value="` + (folderName ? folderName : '') + `"
+                />
+                <input type="text"
+                    placeholder="Config file URI"
+                    class="sync-server"
+                    value="` + (syncServer ? syncServer : '') + `"
+                />
+                Allow JavaScript injection?
+                <input type="checkbox"
+                    class="js-injection-checkbox" ` +
+                    (allowJavaScriptInjection ? 'checked' : '') + `/>
+                <input type="button"
+                    value="Delete"
+                    id="delete-row-button-` + currentRowId + `"
+                />`;
+
+    // The following lines of code add a onclick listener AFTER html is added to DOM.
+    (function (currentRowId) {
+        setTimeout(function() {
+            document.getElementById('delete-row-button-' + currentRowId).addEventListener('click', function () {
+                var syncServersDiv = document.getElementById('sync-servers');
+                var confgRowDiv = document.getElementById('config-row-' + currentRowId);
+                syncServersDiv.removeChild(configRowDiv);
+            });
+        }, 0);
+    })(currentRowId);
+
+    currentRowId++;
+    return configRowDiv;
 }
 
 chrome.storage.local.get('syncServer', function (result) {
@@ -21,7 +58,7 @@ chrome.storage.local.get('syncServer', function (result) {
         var folderName = result.syncServer[i].folderName;
         var syncServer = result.syncServer[i].syncServer;
         var allowJavaScriptInjection = result.syncServer[i].allowJavaScriptInjection;
-        syncServersDiv.innerHTML = syncServersDiv.innerHTML + '<input type="text" placeholder="Folder name (optional)" class="sync-server-folder-name" value="' + (folderName ? folderName : '') + '" /><input type="text" placeholder="Config file URI" class="sync-server" value="' + (syncServer ? syncServer : '') + '"/> Allow JavaScript injection? <input type="checkbox" class="js-injection-checkbox" ' + (allowJavaScriptInjection ? 'checked' : '') + '/><br>';
+        syncServersDiv.appendChild(createConfigRow(folderName, syncServer, allowJavaScriptInjection));
     }
 });
 
@@ -82,43 +119,6 @@ document.getElementById('save-button').addEventListener('click', function () {
 });
 
 document.getElementById('add-row').addEventListener('click', function () {
-    
-    // Collect data.
-    var folderNameElements = document.getElementsByClassName('sync-server-folder-name');
-    var folderNames = [];
-    for (var i=0; i < folderNameElements.length; i++) {
-        folderNames.push(folderNameElements[i].value);
-    }
-    console.debug(folderNames);
-
-    var syncServerElements = document.getElementsByClassName('sync-server');
-    var syncServers = [];
-    for (var i=0; i < syncServerElements.length; i++) {
-        syncServers.push(syncServerElements[i].value);
-    }
-    console.debug(syncServers);
-
-    var allowJavaScriptInjectionCheckboxes = document.getElementsByClassName('js-injection-checkbox');
-    var allowJavaScriptInjectionValues = [];
-    for (var i=0; i < allowJavaScriptInjectionCheckboxes.length; i++) {
-        allowJavaScriptInjectionValues.push(allowJavaScriptInjectionCheckboxes[i].checked);
-    }
-    console.debug(allowJavaScriptInjectionValues);
-
-    // Add field.
     var syncServersDiv = document.getElementById('sync-servers');
-    syncServersDiv.innerHTML = syncServersDiv.innerHTML + '<input type="text" placeholder="Folder name (optional)" class="sync-server-folder-name" /><input type="text" placeholder="Config file URI" class="sync-server" /> Allow JavaScript injection? <input type="checkbox" class="js-injection-checkbox"/><br>';
-
-    // Put data back.
-    folderNameElements = document.getElementsByClassName('sync-server-folder-name');
-    syncServerElements = document.getElementsByClassName('sync-server');
-    allowJavaScriptInjectionCheckboxes = document.getElementsByClassName('js-injection-checkbox');
-
-    for (var i=0; i < syncServers.length; i++) {
-        syncServerElements[i].value = syncServers[i];
-        folderNameElements[i].value = folderNames[i];
-        allowJavaScriptInjectionCheckboxes[i].checked = allowJavaScriptInjectionValues[i];
-
-    }
-
+    syncServersDiv.appendChild(createConfigRow('', '', false));
 });
